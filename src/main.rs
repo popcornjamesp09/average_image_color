57d446c4c45c96daee961387aa58f3d8d7b5058e
James <popcornjamesp09@gmail.com>
2024-12-23 16:22:36 -0500

    First commit

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
 use image::GenericImageView;
-use std::env;
+use walkdir::WalkDir;
+use std::io;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let mut current_work_path: String = String::new();
+    io::stdin().read_line(&mut current_work_path).expect(("Error to read input"));
 
-    if args.len() != 2 {
-        println!("Add the file path to the image you want to be averaged");
-        println!("Usage: {} image.png", args[0]);
-        std::process::exit(0)
+    for file in WalkDir::new(current_work_path).into_iter().filter_map(|file| file.ok()) {
+        let file_name = String::from(file.path().to_str().unwrap());
+        let file_ext = get_file_path_extinsion(file_name.clone(), 3);
+        if file_ext == "png" || file_ext == "jpg" {
+            println!("{}", file_name);
+            generate_image_from_file(file_name)
+        }
     }
+}
 
-    let img_path: &String = &args[1];
+fn generate_image_from_file(img_path: String) {
+    //let img_path_extinsion: String = get_file_path_extinsion((&img_path).to_string(), 4);
 
-    let img_path_extinsion: String = get_img_path_extinsion((&img_path).to_string());
-
-    let img = image::open(img_path).unwrap();
+    let img = image::open(&img_path).unwrap();
 
     let (img_dimension_x, img_dimension_y) = img.dimensions();
 
@@ -22,13 +27,14 @@ fn main() {
 
     let new_image: image::ImageBuffer<image::Rgb<u8>, Vec<_>> = make_image_buf(img_dimension_x, img_dimension_y, color);
 
-    new_image.save(&(img_path.to_string() + ".new" + &img_path_extinsion)).unwrap()
-}
+    new_image.save(img_path).unwrap();
+    //new_image.save(&(img_path.to_string() + ".new" + &img_path_extinsion)).unwrap()
+} 
 
 // This is needed because you have to have a valid image extension for img.save()
 // Is it the best no, does it work yes
-fn get_img_path_extinsion(mut img_path: String) -> String {
-    let len = img_path.chars().count() - 4;
+fn get_file_path_extinsion(mut img_path: String, n: usize) -> String {
+    let len = img_path.chars().count() - n;
     let _ = img_path.drain(0..len).for_each(|_| {});
     return img_path;
 }
@@ -51,10 +57,17 @@ fn get_average_color(img: image::DynamicImage, img_dimension_x: u32, img_dimensi
         }
     }
 
+    println!("r: {}, g: {}, b: {}", r_value, g_value, b_value);
+
     // Needed to find the average color of an image
     let total_pixel_count = img_dimension_x * img_dimension_y;
+
+    let r: u8 = (r_value/total_pixel_count) as u8;
+    let g: u8 = (g_value/total_pixel_count) as u8;
+    let b: u8 = (b_value/total_pixel_count) as u8;
+
     
-    return [(r_value/total_pixel_count) as u8, (g_value/total_pixel_count) as u8, (b_value/total_pixel_count) as u8];
+    return [r, g, b]; // as u8
 
 }
 
@@ -64,7 +77,8 @@ fn make_image_buf(img_dimension_x: u32, img_dimension_y: u32, color: [u8 ; 3]) -
 
     for x in 1..img_dimension_x {
         for y in 1..img_dimension_y {
-            new_image.put_pixel(x, y, image::Rgb(color));
+            new_image.put_pixel(x, y, image::Rgb(color.clone()));
+            println!("{}, {}, {}", color[0], color[1], color[2])
         }
     }
 
